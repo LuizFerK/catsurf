eed01ac6ffb6d66a0ee18873ee29ede2e800ae94
Thaila Schmidt <thailaroani@gmail.com>
2024-11-23 00:00:32 -0300

    header img

--- scenario/fog.js
+++ scenario/fog.js
@@ -1,5 +1,5 @@
 function addFog(scene) {
-  const fogColor = new THREE.Color('#000000')  // Light gray color
+  const fogColor = new THREE.Color('#17001F')  // Light gray color
   const fogNear = 170  // Start of fog
   const fogFar = 240   // End of fog
   scene.fog = new THREE.Fog(fogColor, fogNear, fogFar)
